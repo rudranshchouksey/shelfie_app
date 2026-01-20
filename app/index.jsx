d27e28fd4de4b678183e4a3c7f05047f360bc20e
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2026-01-20 23:43:42 +0530

    Image commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import Logo from '../assets/favicon.png'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+
+      <Text style={styles.title}>Home</Text>
+      <Text style= {{ marginTop: 10, marginBottom: 30 }}>Welcome to Shelfie App!</Text>
+
+      <View>
+        <Text style={styles.card}>Hello, this is a card.</Text>
+      </View>
+    
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  img : {
+    marginVertical: 20,
+  },
+  card: {
+    backgroundColor: '#eee',
+    borderRadius: 8,
+    padding: 16,
+    boxShadow: '4px 4px rgba(0,0,0,0.1)'
+  }
+})
